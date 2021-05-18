f3f2ca77e34e29ad5b3b12558ff992a021224dcc
BogdanMGL <83227580+BogdanMGL@users.noreply.github.com>
2021-05-18 18:18:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.post(`/${telegramToken}`, async function(req, res) {
        
 })  
  
-app.listen(3000 , function() {
+app.listen((process.env.PORT || 3000) , function() {
     console.log('Ready') 
 });
 
